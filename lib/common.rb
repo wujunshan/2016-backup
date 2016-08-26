b98011e127486a197255c1a15b2b1f6f70a471dd
guxiaobai <sikuan.gu@gmail.com>
2016-08-26 17:58:02 +0800

    add rspec

--- lib/common.rb
+++ lib/common.rb
@@ -12,6 +12,15 @@ end
 
 gem_group :development, :test do
   gem 'dotenv-rails', '~> 2.1'
+
+  gem 'rspec-rails', '~> 3.5'
+  gem 'factory_girl_rails', '~> 4.7'
+end
+
+gem_group :test do
+  gem 'database_cleaner', '~> 1.5'
+  gem 'shoulda-matchers', '~> 3.1'
+  gem 'faker', '~> 1.6'
 end
 
 run_bundle
@@ -43,6 +52,46 @@ run "cp .env{,.example}"
 # sidekiq
 prepend_to_file 'config/routes.rb', "require 'sidekiq/web'\n"
 route("mount Sidekiq::Web => '/sidekiq'")
+
+
+# rspec(debug)
+generate 'rspec:install'
+create_file 'spec/support/factory_girl.rb' do
+  <<-EOS.strip_heredoc.indent(2)
+    RSpec.configure do |config|
+      # See http://www.rubydoc.info/gems/factory_girl/file/GETTING_STARTED.md
+      config.include FactoryGirl::Syntax::Methods
+    end
+  EOS
+end
+uncomment_lines 'spec/rails_helper.rb', /support.*require/
+insert_into_file 'spec/rails_helper.rb', :after => "RSpec.configure do |config|\n" do
+  <<-EOS.strip_heredoc.indent(2)
+    Shoulda::Matchers.configure do |config|
+      config.integrate do |with|
+        # Choose a test framework:
+        with.test_framework :rspec
+        with.library :rails
+      end
+    end
+  EOS
+end
+
+inject_into_class 'config/application.rb', 'Application' do
+  <<-EOS.strip_heredoc.indent(4)
+    # Setting for Rspec
+    config.generators do |g|
+      g.test_framework :rspec,
+        fixtures: true,
+        view_specs: false,
+        helper_spece: false,
+        routing_specs: false,
+        controller_specs: false,
+        request_specs: false
+      g.fixture_replacement :factory_girl, dir: 'spec/factories'
+    end
+  EOS
+end
 
 git add: '.'
 git commit: %Q< -m 'oh-my-rails: common' >

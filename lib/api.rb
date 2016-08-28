6cfe7bcef4b02f71fc72558b854d416690398112
guxiaobai <sikuan.gu@gmail.com>
2016-08-29 03:22:36 +0800

    add ams

--- lib/api.rb
+++ lib/api.rb
@@ -1,5 +1,15 @@
+gem 'active_model_serializers', '~> 0.10.2'
+
 run_bundle
 run "spring binstub --remove --all"
+
+# ActiveModelSerializers
+create_file 'config/initializers/json_api.rb' do
+  <<-EOS.strip_heredoc
+    ActiveModel::Serializer.config.adapter = :json_api
+    Mime::Type.register "application/json", :json, %w( text/x-json application/jsonrequest application/vnd.api+json )
+  EOS
+end
 
 run "bundle exec spring binstub  --all"
 git add: '.'

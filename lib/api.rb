1f96d27190aa52e2d85d3d17951d8fe7c9258b63
guxiaobai <sikuan.gu@gmail.com>
2016-08-29 16:31:09 +0800

    add cors rack

--- lib/api.rb
+++ lib/api.rb
@@ -1,4 +1,5 @@
 gem 'active_model_serializers', '~> 0.10.2'
+gem 'rack-cors', '~> 0.4.0'
 
 run_bundle
 run "bundle exec spring stop"
@@ -9,6 +10,21 @@ create_file 'config/initializers/json_api.rb' do
   <<-EOS.strip_heredoc
     ActiveModel::Serializer.config.adapter = :json_api
     Mime::Type.register "application/json", :json, %w( text/x-json application/jsonrequest application/vnd.api+json )
+  EOS
+end
+
+
+# Rack CORS Middleware
+application do
+  <<-EOS.strip_heredoc.indent(4)
+
+    # Rack CORS Middleware
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins '*'
+        resource '*', :headers => :any, :methods => [:get, :post, :options]
+      end
+    end
   EOS
 end
 
